81a7b2820b1d42e83e00d8c610ea3d3d7d7d678d
Roothex200 <sabiturrahman60@gmail.com>
2025-03-15 00:19:20 +0600

    modify scannerhistroylist adapter to show readable data from database

--- app/src/main/java/com/example/scannerapp/view/landing/history/adapter/ScanHistoryListAdapter.kt
+++ app/src/main/java/com/example/scannerapp/view/landing/history/adapter/ScanHistoryListAdapter.kt
@@ -1,20 +1,31 @@
 package com.example.scannerapp.view.landing.history.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.scannerapp.R
+import com.example.scannerapp.db.QRHistoryDao
+import com.example.scannerapp.db.QRHistoryInfo
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-class ScanHistoryListAdapter: RecyclerView.Adapter<ScanHistoryListAdapter.ViewHolder>() {
+class ScanHistoryListAdapter(
+    var historyList:List<QRHistoryInfo>,
+    var qrHistoryDao: QRHistoryDao): RecyclerView.Adapter<ScanHistoryListAdapter.ViewHolder>() {
 
 
     class ViewHolder(view: View):RecyclerView.ViewHolder(view) {
         var text=view.findViewById<TextView>(R.id.linkText)
         var data=view.findViewById<TextView>(R.id.data)
         var deleteBtn=view.findViewById<ImageView>(R.id.deleteBtn)
+        var dateTimeview=view.findViewById<TextView>(R.id.dateTime)
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,10 +34,21 @@ class ScanHistoryListAdapter: RecyclerView.Adapter<ScanHistoryListAdapter.ViewHo
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return historyList.size
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.text.setText("sabitur rahman ${position}")
+        var parse=LocalDateTime.parse(historyList[position].createAt)
+        val formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy hh:mm a")
+        val formateDate = parse.format(formatter)
+
+        holder.dateTimeview.setText(formateDate)
+        holder.text.setText("Data")
+        holder.data.setText(historyList[position].value)
+
+        holder.deleteBtn.setOnClickListener {
+            qrHistoryDao.delete(historyList[position])
+        }
     }
 }
