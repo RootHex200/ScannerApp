37946b83eea6e8d2a98d1dfe31879f555c048dd3
Roothex200 <sabiturrahman60@gmail.com>
2025-04-08 15:33:07 +0600

    add wifi qr generator fragment

---
+++ app/src/main/java/com/example/scannerapp/view/landing/QrGenerator/QrGeneratorOption/WifiQrFragment.kt
@@ -0,0 +1,80 @@
+package com.example.scannerapp.view.landing.QrGenerator.QrGeneratorOption
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidmads.library.qrgenearator.QRGContents
+import com.example.scannerapp.R
+import com.example.scannerapp.service.QRGeneratorService
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WifiQrFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WifiQrFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var view:View=inflater.inflate(R.layout.fragment_wifi_qr, container, false);
+
+        var ssidInput=view.findViewById<EditText>(R.id.ssidInput);
+        var passwordInput=view.findViewById<EditText>(R.id.passwordInput);
+        var generateQrButton=view.findViewById<LinearLayout>(R.id.generateQrButton);
+        var qrImageview=view.findViewById<ImageView>(R.id.qrImageview);
+
+        generateQrButton.setOnClickListener {
+            Log.d("WifiQrFragment.setOnClickListener","click")
+            val getBitmap=QRGeneratorService().generateQR(inputValue = ssidInput.text.toString(), Qrtype = QRGContents.Type.TEXT)
+            qrImageview.setImageBitmap(getBitmap);
+
+        }
+        return view;
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment wifi_qr.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            WifiQrFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
